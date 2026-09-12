671015a156263bc65bc536316a9a83dd348ed7d4
austincoutinhoog-stack <austincoutinhoog@gmail.com>
2026-09-12 23:20:44 +0530

    Step 3: Register SignalDetectorPlugin in MainActivity.java

--- android/app/src/main/java/com/kragvor/app/MainActivity.java
+++ android/app/src/main/java/com/kragvor/app/MainActivity.java
@@ -1,10 +1,13 @@
 package com.kragvor.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.kragvor.signal.SignalDetectorPlugin;
 
 public class MainActivity extends BridgeActivity {
 	@Override
-	public void onCreate(android.os.Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
+		registerPlugin(SignalDetectorPlugin.class);
 		super.onCreate(savedInstanceState);
 	}
 }
